4130c1e1ce071fd511e2dd6c0ed1fd52d843b8ea
AndriiHamasa <andrii.khamaza@nure.ua>
2023-03-21 12:31:47 +0300

    fixed error in first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,8 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
   
-btnStartEl = document.querySelector('[data-start]');
-btnStopEl = document.querySelector('[data-stop]');
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
 
 console.log(btnStartEl);
 
